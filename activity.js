c086d40e46db477c93489c5a34890ff3fe8f9742
Imoestoe <59838287+Imoestoe@users.noreply.github.com>
2020-01-13 18:42:20 +0100

    w

--- activity.js
+++
@@ -1,7 +0,0 @@
-module.exports = bot => {
-
-
-
-let activities = [ `${bot.users.size} users!` ], i = 0;
-setInterval;{() => bot.user.setActivity(`${bot.prefix}help | ${activities[i++ % activities.length]}`, { type: "WATCHING" }), 15000;
-}};
