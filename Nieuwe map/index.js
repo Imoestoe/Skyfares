aa46826d5bf412e3b0598eff00b00ee31c2842e8
Imoestoe <59838287+Imoestoe@users.noreply.github.com>
2020-01-13 17:42:58 +0100

    Init

---
+++ Nieuwe map/index.js
@@ -0,0 +1,12 @@
+const discord = require("discord.js");
+const botConfig = require("./botconfig.json");
+
+const bot = new discord.Client();
+
+bot.on("ready", async () => {
+
+    console.log(`${bot.user.username} is online!`)
+
+})
+
+bot.login(process.env.token);
