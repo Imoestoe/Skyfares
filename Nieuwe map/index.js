3e34925f3c3fe6ebe02a399b7734f847a8970bd5
Imoestoe <59838287+Imoestoe@users.noreply.github.com>
2020-01-14 15:58:57 +0100

    w

--- Nieuwe map/index.js
+++ Nieuwe map/index.js
@@ -8,8 +8,8 @@ bot.on("ready", async () => {
     console.log(`${bot.user.username} is online!`)
 
 
-      let activities = [ `${bot.guilds.memberCount} users!`], i = 0
-      setInterval(() => bot.user.setActivity(`!help | ${activities[i++ % activities.length]}`, { type: "WATCHING" }), 15000)
+      let activities = [ `${bot.users.size} users!` , `play.skyfares.net` ], i = 0
+      setInterval(() => bot.user.setActivity(`${activities[i++ % activities.length]}`, { type: "WATCHING" }), 15000)
 })
 
 bot.login(process.env.token);
