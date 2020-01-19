3947844b7c98f035d18d58824e5cb263a5907f79
Imoestoe <59838287+Imoestoe@users.noreply.github.com>
2020-01-19 12:45:08 +0100

    wadasd

--- Nieuwe map/Commands/testG.js
+++ Nieuwe map/Commands/testG.js
@@ -1,113 +1,152 @@
-const discord = require("discord.js");
- 
-module.exports.run = async (bot, message, args) => {
- 
-    // Argumenten die we later nodig hebben.
-    var item = "";
-    var picture = "";
-    var time;
-    var winnerCount;
- 
-    // Nakijken als je perms hebt om dit command te doen.
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Sorry jij kan dit niet doen");
- 
-    // !giveaway aantalWinnaars seconden itemOmTeWinnen.
- 
-    // Aantal winnaars opvragen.
-    winnerCount = args[0];
-    // Tijd hoelang het moet duren.
-    time = args[1];
+class Giveaway {
+    constructor(client, message, match, data) {
+        const [full, channel, winnerCount, timeInSeconds, days, hours, minutes, seconds, title] = match;
 
-    picture = args[2];
+        this.client = client;
 
-    item = args.splice(3, args.length).join(' ');
- 
-    // Verwijder het bericht dat net is gemaakt door de gebruiker.
-    message.delete();
- 
-    // Verval datum berekenen.
-    var date = new Date().getTime();
-    var dateTime = new Date(date + (time * 1000));
- 
-    // Maak embed aan.
-    var giveawayEmbed = new discord.RichEmbed()
-        .setTitle("🎉 **GIVEAWAY** 🎉")
-        .setFooter(`Vervalt: ${dateTime}`)
-        .setDescription("**To win: **")(item)
-        .setThumbnail(picture);
- 
-    // Verzend embed en zet de reactie op de popper.
-    var embedSend = await message.channel.send(giveawayEmbed);
-    embedSend.react("🎉");
- 
-    // Zet een timeout die na het aantal seconden af gaat.
-    setTimeout(function () {
- 
-        // Argumenten die we nodig hebben.
-        var random = 0;
-        var winners = [];
-        var inList = false;
- 
-        // Verkrijg de gebruikers die gereageerd hebben op de giveaway.
-        var peopleReacted = embedSend.reactions.get("🎉").users.array();
- 
-        // Hier gaan we al de mensen over gaan en kijken als de bot er tussen staan
-        // De bot moeten we uit de lijst weghalen en dan gaan we verder.
-        for (var i = 0; i < peopleReacted.length; i++) {
-            if (peopleReacted[i].id == bot.user.id) {
-                peopleReacted.splice(i, 1);
-                continue;
+        this.message = message;
+
+        this.channel = data ? this.client.guilds.get(data.guild).channels.get(data.channel) : message.guild.channels.get(channel) || message.channel;
+
+        this.winnerCount = winnerCount || 1;
+
+        this.length = timeInSeconds ? timeInSeconds : (60 * 60 * 24 * (days ? Number(days) : 0)) + (60 * 60 * (hours ? Number(hours) : 0)) + (60 * (minutes ? Number(minutes) : 0)) + (seconds ? Number(seconds) : 0);
+
+        this.remaining = data ? data.remaining : this.length;
+
+        this.endTime = Date.now() + this.length * 1000;
+
+        this.title = title;
+
+        this.winners = [];
+
+        this.msg;
+
+        this.interval;
+
+        this.suffix = this.client.giveawayCache.filter(giveaway => giveaway.channel.id === this.channel.id).size+1;
+
+        this.match = match;
+
+        this.valid = true;
+
+        if (data) { this.continue(data); } else { this.init(); }
+    }
+
+    async init() {
+        this.msg = await this.channel.buildEmbed()
+            .setColor(0xcc8822)
+            .setTitle(this.title)
+            .setDescription(`Possible Winners: ${this.winnerCount}\nReact with :tada: to enter!`)
+            .setFooter(`${this.suffix} | Ends at`)
+            .setTimestamp(new Date(this.endTime))
+            .send();
+
+        this.msg.react("🎉");
+
+        this.interval = setInterval(() => this.intervalFunction(), 1000);
+
+        this.client.giveawayCache.set(`${this.msg.channel.id}-${this.suffix}`, this);
+    }
+
+    async continue(data) {
+        this.msg = await this.client.channels.get(data.channel).messages.fetch(data.msg).catch(() => {
+            clearInterval(this.interval);
+            this.client.lastGiveawayCache.set(`${this.msg.channel.id}-${this.suffix}`, this);
+            this.client.giveawayCache.delete(`${this.msg.channel.id}-${this.suffix}`);
+        });
+        this.winnerCount = await data.winnerCount;
+        this.title = await data.title;
+        this.interval = setInterval(() => this.intervalFunction(), 1000);
+
+        this.client.giveawayCache.set(`${this.msg.channel.id}-${this.suffix}`);
+    }
+
+    async intervalFunction() {
+        this.remaining--;
+
+        this.channel.messages.fetch(this.msg.id).catch(() => {
+            clearInterval(this.interval);
+            this.client.lastGiveawayCache.set(`${this.msg.channel.id}-${this.suffix}`, this);
+            this.client.giveawayCache.delete(`${this.msg.channel.id}-${this.suffix}`);
+        });
+
+        if(Date.now() >= this.endTime){
+            clearInterval(this.interval);
+            this.client.lastGiveawayCache.set(`${this.msg.channel.id}-${this.suffix}`, this);
+            this.client.giveawayCache.delete(`${this.msg.channel.id}-${this.suffix}`);
+
+            const embed = this.msg.embeds[0];
+            embed.color = 0x171c23;
+
+            //const users = await this.msg.reactions.get("%F0%9F%8E%89").users.fetch();
+            const users = await this.msg.reactions.get("🎉").users.fetch();
+            const list = users.array().filter(u => u.id !== this.msg.author.id);
+
+            if (!list.length) {
+                embed.description = `Winner: No one.`;
+                embed.footer.text = `Giveaway Finished`;
+
+                return this.msg.edit({ embed });
             }
-        }
- 
-        // Hier kijken we na als er wel iemand heeft meegedaan.
-        if (peopleReacted.length == 0) {
-            return message.channel.send("Niemand heeft gewonnen dus de bot wint.");
-        }
- 
-        // Tijdelijk kijken we na als er te wienig mensen hebben mee gedaan aan de wedstrijd.
-        if (peopleReacted.length < winnerCount) {
-            return message.channel.send("Er zijn te weinig mensen die mee deden daarom heeft de bot gewonnen.");
-        }
- 
-        // Voor het aantal winnaars dat we eerder hebben opgegeven gaan we een random nummer aanmaken en de user in een array zetten.
-        for (var i = 0; i < winnerCount; i++) {
- 
-            inList = false;
- 
-            // Aanmaken van een random getal zodat we een user kunnen kiezen.
-            random = Math.floor(Math.random() * peopleReacted.length);
- 
-            // Als een winnaar al voorkomt in de winnaars lijst dan moeten we opnieuw gaan zoeken naar een andere winnaar.
-            for (var y = 0; y < winners.length; y++) {
-                // Nakijken als de geslecteerde winnaar al in de lijst zit.
-                if (winners[y] == peopleReacted[random]) {
-                    // We zetten i 1 minder zodat we opnieuw kunnen doorgaan in de lijst.
-                    i--;
-                    // We zetten dit op true zodat we weten dat deze al in de lijst zit.
-                    inList = true;
-                    break;
-                }
+
+            for (let i = 0; i < this.winnerCount; i++) {
+                const x = this.draw(list);
+
+                if (!this.winners.includes(x)) this.winners.push(x);
             }
- 
-            // Zit deze niet in de lijst gaan we deze toevoegen.
-            if (!inList) {
-                winners.push(peopleReacted[random]);
+
+            embed.description = `Winner: ${this.winners.filter(u => u !== undefined && u !== null).map(u => u.toString()).join(", ")}`;
+            embed.footer.text = `${this.suffix} | Giveaway Finished`;
+
+            this.msg.edit({ embed });
+
+            if (this.winners.length) this.msg.channel.send(`Congratulations, ${this.winners.map(u => u.toString()).join(", ")}! You won the giveaway for **${this.title}**!`);
+        }
+
+        /*if (this.remaining >= 5 && this.remaining % 5 === 0) {
+            const embed = this.msg.embeds[0];
+            embed.footer.text = `Time Remaining: ${time}`;
+            this.msg.edit({ embed });
+        } else if (this.remaining < 5 && this.remaining > 0) {
+            const embed = this.msg.embeds[0];
+            embed.footer.text = `Time Remaining: ${time}`;
+            this.msg.edit({ embed });
+        } else if (this.remaining === 0) {
+            clearInterval(this.interval);
+            this.client.lastGiveawayCache.set(this.msg.channel.id, this);
+            this.client.giveawayCache.delete(this.msg.channel.id);
+            const embed = this.msg.embeds[0];
+            embed.color = 0x171c23;
+            //const users = await this.msg.reactions.get("%F0%9F%8E%89").users.fetch();
+            const users = await this.msg.reactions.get("🎉").fetchUsers();
+            const list = users.filterArray(u => u.id !== this.msg.author.id);
+            if (!list.length) {
+                embed.description = `Winner: No one.`;
+                embed.footer.text = `Giveaway Finished`;
+                return this.msg.edit({ embed });
             }
- 
+            for (let i = 0; i < this.winnerCount; i++) {
+                const x = this.draw(list);
+                if (!this.winners.includes(x)) this.winners.push(x);
+            }
+            embed.description = `Winner: ${this.winners.filter(u => u !== undefined && u !== null).map(u => u.toString()).join(", ")}`;
+            embed.footer.text = `Giveaway Finished`;
+            this.msg.edit({ embed });
+            if (this.winners.length) this.msg.channel.send(`Congratulations, ${this.winners.map(u => u.toString()).join(", ")}! You won the giveaway for **${this.title}**!`);
+        }*/
+    }
+
+    shuffle(arr) {
+        for (let i = arr.length; i; i--) {
+            const j = Math.floor(Math.random() * i);
+            [arr[i - 1], arr[j]] = [arr[j], arr[i - 1]];
         }
- 
-        // Voor iedere winnaar gaan we een bericht sturen.
-        for (var i = 0; i < winners.length; i++) {
-            message.channel.send("Proficiat " + winners[i] + `! Je hebt gewonnen **${item}**.`);
-        }
- 
-    }, 1000 * time);
- 
- 
-}
- 
-module.exports.help = {
-    name: "giveaway",
-    description: "Start een giveaway"
+        return arr;
+    }
+
+    draw(list) {
+        const shuffled = this.shuffle(list);
+        return shuffled[Math.floor(Math.random() * shuffled.length)];
+    }
 }
