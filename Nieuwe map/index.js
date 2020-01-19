4023c74624ffef90b1f2d4103ba60c7e8d6199d3
Imoestoe <59838287+Imoestoe@users.noreply.github.com>
2020-01-19 19:23:05 +0100

    awdwa

--- Nieuwe map/index.js
+++ Nieuwe map/index.js
@@ -47,7 +47,7 @@ bot.on("ready", async () => {
 
       let activities = [ `!help | ${bot.users.size-1} users` , `play.skyfarers.net` ], i = 0;
       setInterval(() => bot.user.setActivity(`${activities[i++ % activities.length]}`, { type: "WATCHING" }), 15000)
-})
+});
 if(message.channel.type == "text" && message.channel.name.toLowerCase() == "suggestion"){
   message.react("✅");
   message.react("❌");
