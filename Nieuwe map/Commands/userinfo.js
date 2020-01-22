8dd91bc66011e235486dbf63922f78ecac91fd52
Imoestoe <59838287+Imoestoe@users.noreply.github.com>
2020-01-22 17:25:04 +0100

    Update userinfo.js

--- Nieuwe map/Commands/userinfo.js
+++ Nieuwe map/Commands/userinfo.js
@@ -1,23 +1,23 @@
 const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let inline = true
-    let resence = true
+    let inline = true;
+    let resence = true;
     const status = {
         online: "<:online:424890369688469504> Online",
         idle: "<:idle:424890472855502849> Idle",
         dnd: "<:dnd:424890429524410368> Do Not Disturb",
         offline: "<:offilne:424890400319340546> Offline/Invisible"
-      }
+      };
         
-const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
-let target = message.mentions.users.first() || message.author
+const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;;
+let target = message.mentions.users.first() || message.author;
 
 if (member.user.bot === true) {
     bot = "<:bottag:425631858265423883> Yes";
   } else {
     bot = "<:user:424958082691629057> No";
-  }
+  };
 
             let embed = new Discord.RichEmbed()
                 //.setAuthor(member.user.username)
@@ -32,7 +32,7 @@ if (member.user.bot === true) {
                 .addField("Roles", `${member.roles.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "<:no:425632070036094986> No Roles"}`, true)
                 .addField("Joined Discord At", member.user.createdAt)
                 .setFooter(`Information about ${member.user.username}`)
-                .setTimestamp()
+                .setTimestamp();
     
             message.channel.send(embed);
 
