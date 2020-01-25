4de2b79c7f0ba911279df5da3bc6f726c459d03b
Imoestoe <59838287+Imoestoe@users.noreply.github.com>
2020-01-25 19:08:17 +0100

    wQS

--- Nieuwe map/index.js
+++ Nieuwe map/index.js
@@ -45,7 +45,32 @@ if(commands) commands.run(bot, message, args);
 
 });
 
+<<<<<<< HEAD
 
+=======
+bot.on('message', message => {
+  if(message.content === "!invites"){
+  var user = null;
+  user = message.author;
+
+  message.guild.fetchInvites()
+  .then
+
+  (invites =>
+      {
+          const userInvites = invites.array().filter(o => o.inviter.id === user.id);
+          var userInviteCount = 0;
+              for(var i=0; i < userInvites.length; i++)
+              {
+                  var invite = userInvites[i];
+                  userInviteCount += invite['uses'];
+              }
+                  message.reply(`You have invited ${userInviteCount} user(s) to this server. Keep up the good work!`);
+      }
+  )
+}
+});
+>>>>>>> parent of 2ee253b... awdawd
 
 
 //bot.on('message', message => {
