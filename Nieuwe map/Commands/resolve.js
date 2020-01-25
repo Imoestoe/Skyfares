1dd4488688baa82fadd129abc9738ecd729019b5
Imoestoe <59838287+Imoestoe@users.noreply.github.com>
2020-01-25 19:48:00 +0100

    awdawd

---
+++ Nieuwe map/Commands/resolve.js
@@ -0,0 +1,26 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    message.delete(1000);
+        try {
+
+    var text = {
+        "embed": {
+            "title": "Bug's-Resolved",
+            "description": "Files reloaded",
+            "footer": {
+                "text": "Skyfarers ● Admin-system"
+            },
+        }};
+    message.author.send(text);
+
+    message.channel.send("This command has shown in your DM");
+
+    } catch (error) {
+        message.channel.send("Error: This command is out of service");
+    }
+
+}
+module.exports.help = {
+name: "help"
+}
