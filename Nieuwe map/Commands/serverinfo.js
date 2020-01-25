f24364561e202c2aa7efe1c92d8ebcd4322ed53c
Imoestoe <59838287+Imoestoe@users.noreply.github.com>
2020-01-25 19:34:03 +0100

    wdadw

--- Nieuwe map/Commands/serverinfo.js
+++ Nieuwe map/Commands/serverinfo.js
@@ -41,6 +41,5 @@ module.exports.run = async (bot, message, args) => {
 }
 
 module.exports.help = {
-    name: "serverinfo",
-    description: "server"
+  name:"serverinfo"
 }
--- Nieuwe map/Commands/test.js
+++ Nieuwe map/Commands/test.js
@@ -23,6 +23,5 @@ module.exports.run = async (bot, message, args) => {
 
 }
 module.exports.help = {
-    name: "help",
-    description: "helpt mensen"
+name: "help"
 }
