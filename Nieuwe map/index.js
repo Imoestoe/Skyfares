f4dd91223a9ba170a7b3ce75285b80b11431fc67
Imoestoe <59838287+Imoestoe@users.noreply.github.com>
2020-01-28 17:45:35 +0100

    wdawd

--- Nieuwe map/index.js
+++ Nieuwe map/index.js
@@ -6,7 +6,6 @@ const fs = require("fs");
 bot.commands = new discord.Collection();
 
 
-
 fs.readdir("./Commands/", (err, files) => {
 
   if (err) console.log(err);
@@ -44,7 +43,28 @@ if(commands) commands.run(bot, message, arguments);
 
 });
 
+bot.on('message', message => {
+  if(message.content === "!invites"){
+  var user = null;
+  user = message.author;
 
+  message.guild.fetchInvites()
+  .then
+
+  (invites =>
+      {
+          const userInvites = invites.array().filter(o => o.inviter.id === user.id);
+          var userInviteCount = 0;
+              for(var i=0; i < userInvites.length; i++)
+              {
+                  var invite = userInvites[i];
+                  userInviteCount += invite['uses'];
+              }
+                  message.reply(`You have invited ${userInviteCount} user(s) to this server.`);
+      }
+  )
+}
+});
 
 
 //bot.on('message', message => {
