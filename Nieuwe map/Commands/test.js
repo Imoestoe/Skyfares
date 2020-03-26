88f957d2b84b8e18caa6c9270034d9546abc427b
Imoestoe <59838287+Imoestoe@users.noreply.github.com>
2020-03-26 19:52:14 +0100

    awdawd

--- Nieuwe map/Commands/test.js
+++ Nieuwe map/Commands/test.js
@@ -7,15 +7,15 @@ module.exports.run = async (bot, message, args) => {
     var text = {
         "embed": {
             "title": "Help menu",
-            "description": "**Commands:**\n\n!Ticket\n`This will create a channel in the Discord, which only staff can see.`\n` You can report players and bugs here without any other player noticing it.`\n\n!Help\n`See all commands.`\n\n\n!Test\n`Command discription`\n\n\n!Test\n`Command discription`",
+            "description": "**Commands:**\n\n!Ticket\n`Dit maakt een prive tekst kanaal aan, waar je kan worden geholpen door staff.`\n` Je kan bugs en spelers raporteren zonder dat iemand het door heeft!`\n\n!Help\n`Zie alle commando's.`\n\n\n!Test\n`Command discription`\n\n\n!Test\n`Command discription`",
             "footer": {
-                "text": "Skyfarers ● Help-system"
+                "text": "Blockville ● Help-system"
             },
             "color": 8405687
         }};
     message.author.send(text);
 
-    message.channel.send("This command has shown in your DM");
+    message.channel.send("Alle commando's zijn in je privé berichten gestuurd. Zie je dit niet? Zet bij de server even je prive berichten aan, zie #informatie voor instructie.");
 
     } catch (error) {
         message.channel.send("Error: This command is out of service");
