87453bd85cc5e1643d4e66bb6647bc0e8ffaaa99
Imoestoe <59838287+Imoestoe@users.noreply.github.com>
2020-03-27 10:53:30 +0100

    awdawd

--- Nieuwe map/Commands/ticket.js
+++ Nieuwe map/Commands/ticket.js
@@ -37,7 +37,7 @@ module.exports.run = async (bot, message, args) => {
     message.channel.send(embedCreateTicket);
  
     // Maak kanaal en zet in juiste categorie.
-    message.guild.createChannel("🎫-" + userName + "-" + userDiscriminator, "text").then((createdChan) => { // Maak kanaal
+    message.guild.createChannel( "🎫-" + userName + "-" + userDiscriminator, "text").then((createdChan) => { // Maak kanaal
  
         createdChan.setParent(categoryId).then((settedParent) => { // Zet kanaal in category.
  
