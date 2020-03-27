53dd1d60358a2f119f046706dba448d2ba50dc55
Imoestoe <59838287+Imoestoe@users.noreply.github.com>
2020-03-27 10:42:01 +0100

    awdawd

--- Nieuwe map/Commands/ticket.js
+++ Nieuwe map/Commands/ticket.js
@@ -43,7 +43,7 @@ module.exports.run = async (bot, message, args) => {
  
             // Zet perms voor iedereen
             settedParent.overwritePermissions(message.guild.roles.find('name', "@everyone"), { "READ_MESSAGES": false });
-            settedParent.overwritePermissions(message.guild.roles.find('name', "@Ticket Team"), {                 
+            settedParent.overwritePermissions(message.guild.roles.find('name', "Ticket Team"), {                 
                 "READ_MESSAGES": true, "SEND_MESSAGES": true,
                 "ATTACH_FILES": true, "CONNECT": true,
                 "CREATE_INSTANT_INVITE": false, "ADD_REACTIONS": true 
