1d99cacaa0fd8a8a18c503260b3fe8b805f66302
Imoestoe <59838287+Imoestoe@users.noreply.github.com>
2020-04-12 17:28:31 +0200

    wdad

--- Nieuwe map/Commands/ticketclose.js
+++ Nieuwe map/Commands/ticketclose.js
@@ -3,7 +3,7 @@ const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
     // Id van category van tickets.
-    const categoryId = "692803460248371380";
+    const categoryId = "698916608520880129";
  
     // Als bericht in ticket kanaal is dan verwijder kanaal ander zend bericht
     if (message.channel.parentID == categoryId) {
